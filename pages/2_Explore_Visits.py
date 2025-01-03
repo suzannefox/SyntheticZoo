6509d8a4ae718844d225b4fccd230b53cdd7a81e
suzannefox <suzanne_fox@hotmail.com>
2025-01-03 19:37:18 +0000

    Update Visits

--- pages/2_Explore_Visits.py
+++ pages/2_Explore_Visits.py
@@ -13,43 +13,43 @@ fig = px.line(df_Calendar, x="date", y="visits", title="Visits Over the Year 202
               labels={"date": "Date", "visits": "Number Of Visits", "weekend": "Weekend"},
               hover_data={"weekend": True} )
     
-fig.update_layout(
-    xaxis_title="Date",
-    yaxis_title="Visits",
-    template="plotly_white",  # Optional: cleaner template
-    xaxis=dict(rangeslider=dict(visible=True))  # Enable range slider
-)
+# fig.update_layout(
+#     xaxis_title="Date",
+#     yaxis_title="Visits",
+#     template="plotly_white",  # Optional: cleaner template
+#     xaxis=dict(rangeslider=dict(visible=True))  # Enable range slider
+# )
 
-# Mark Bank Holidays
-for date in df_Calendar[df_Calendar["type"] != 'Normal Day']["date"]:
+# # Mark Bank Holidays
+# for date in df_Calendar[df_Calendar["type"] != 'Normal Day']["date"]:
 
-    date1 = datetime.datetime.strptime(date, '%Y-%m-%d')
-    date2 = date1 + timedelta(days=1)
-    date2 = date2.strftime("%Y-%m-%d")
+#     date1 = datetime.datetime.strptime(date, '%Y-%m-%d')
+#     date2 = date1 + timedelta(days=1)
+#     date2 = date2.strftime("%Y-%m-%d")
     
-    fig.add_vrect(
-        x0=date,
-        x1=date2,
-        label=dict(
-            text="Bank Holiday",
-            textposition="top center",
-        ),
-        fillcolor="green",
-        opacity=0.25,
-        line_width=0,
-    )
+#     fig.add_vrect(
+#         x0=date,
+#         x1=date2,
+#         label=dict(
+#             text="Bank Holiday",
+#             textposition="top center",
+#         ),
+#         fillcolor="green",
+#         opacity=0.25,
+#         line_width=0,
+#     )
 
-# Customize the range slider with a light grey background
-fig.update_layout(
-    template="plotly_white",
-    xaxis=dict(
-        title="Date Range Slider",
-        rangeslider=dict(
-            visible=True,         # Enable the range slider
-            bgcolor="lightgrey",  # Set the background color of the slider
-        ),
-    ),
-    yaxis=dict(title="Number of Visits"),
-)
+# # Customize the range slider with a light grey background
+# fig.update_layout(
+#     template="plotly_white",
+#     xaxis=dict(
+#         title="Date Range Slider",
+#         rangeslider=dict(
+#             visible=True,         # Enable the range slider
+#             bgcolor="lightgrey",  # Set the background color of the slider
+#         ),
+#     ),
+#     yaxis=dict(title="Number of Visits"),
+# )
 
 st.plotly_chart(fig, use_container_width=True)
