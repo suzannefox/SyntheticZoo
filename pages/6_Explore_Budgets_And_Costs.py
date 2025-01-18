27120bbc786a2ff6ce3dc07ae381af0ac1a5c695
suzannefox <suzanne_fox@hotmail.com>
2025-01-18 22:00:34 +0000

    WebSite Update

--- pages/6_Explore_Budgets_And_Costs.py
+++ pages/6_Explore_Budgets_And_Costs.py
@@ -32,27 +32,68 @@ def render_dataframe_with_highlight(df, highlight_col_idx=1, highlight_color='li
     html_table += "</tbody>\n</table>"
     return html_table 
 
-
 # ------------------------------------------------------------------------------------
 df_Invoices = pd.read_csv(f'./data/Zoo-Invoices.csv')
 df_Budgets = pd.read_csv(f'./data/Zoo-Budgets.csv')
 
+
+# ------------------------------------------------------------------------------------
+st.subheader('Annual Budgets')
+
 # Group by 'Month' and 'Order', then sum the 'Budget'
 df_grouped = df_Budgets.groupby(['Month', 'Order Type'], as_index=False).sum()
-
 # Pivot the DataFrame to wide format
 df_result = df_grouped.pivot(index='Month', columns='Order Type', values='Budget').reset_index()
+# Optionally, clean up column names (remove MultiIndex if present)
+df_result.columns.name = None
+df_result['Total'] = df_result.iloc[:, 1:].sum(axis=1)
+df_result = df_result[['Month','Total','Coffee', 'Tea', 'Meat','Fish','Fruit','Plants']]
+df_result = df_result.applymap(lambda x: f"£{x:,.2f}" if isinstance(x, (int, float)) else x)
 
+html_result = render_dataframe_with_highlight(df_result, highlight_col_idx=1, highlight_color='lightgrey')
+# Render the styled table in Streamlit
+# Add CSS for scrollable div with specified height
+html_table_with_scroll = f"""
+<div style="height: 200px; overflow-y: scroll; border: 1px solid #ccc; padding: 10px;">
+    {html_result}
+</div>
+"""
+st.markdown(html_table_with_scroll, unsafe_allow_html=True)
+
+# ------------------------------------------------------------------------------------
+st.subheader('Annual Costs')
+
+# Group by 'Month' and 'Order', then sum the 'Budget'
+df_grouped = df_Invoices.groupby(['Month', 'Order Type'], as_index=False).sum()
+# Pivot the DataFrame to wide format
+df_result = df_grouped.pivot(index='Month', columns='Order Type', values='Cost').reset_index()
 # Optionally, clean up column names (remove MultiIndex if present)
 df_result.columns.name = None
 df_result['Total'] = df_result.iloc[:, 1:].sum(axis=1)
 df_result = df_result[['Month','Total','Coffee', 'Tea', 'Meat','Fish','Fruit','Plants']]
 
-# ------------------------------------------------------------------------------------
-st.subheader('Monthly Budgets')
+df_result = df_result.applymap(lambda x: f"£{x:,.2f}" if isinstance(x, (int, float)) else x)
 
-df_result = df_result.astype(str)
 html_result = render_dataframe_with_highlight(df_result, highlight_col_idx=1, highlight_color='lightgrey')
 # Render the styled table in Streamlit
-st.markdown(html_result, unsafe_allow_html=True)
+# Add CSS for scrollable div with specified height
+html_table_with_scroll = f"""
+<div style="height: 200px; overflow-y: scroll; border: 1px solid #ccc; padding: 10px;">
+    {html_result}
+</div>
+"""
+st.markdown(html_table_with_scroll, unsafe_allow_html=True)
+
+# ------------------------------------------------------------------------------------
+st.subheader('Monthly Budget Allocation by Enclosure')
+
+df_Budgets['Text'] = df_Budgets.apply(lambda row: f"{row['Enclosure']} (£{row['Budget']:.0f})", axis=1)
+
+df_Summary = df_Budgets[df_Budgets['Month'] == '2024-01'].groupby(['Order Type']).agg(
+    Total=('Budget','sum'),
+    Enclosures=('Text', lambda x: ', '.join(x.astype(str))))
+df_Summary['Total'] = df_Summary['Total'].apply(lambda x: f"£{x:,.0f}")
+df_Summary = df_Summary.loc[['Coffee', 'Tea', 'Meat', 'Fish', 'Fruit', 'Plants']]
+
+st.write(df_Summary)
 
