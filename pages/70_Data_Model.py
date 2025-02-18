a4140518950e7c4e10728a27bdf6261ea478236d
suzannefox <suzanne_fox@hotmail.com>
2025-02-18 12:16:17 +0000

    Add Data Model

---
+++ pages/70_Data_Model.py
@@ -0,0 +1,9 @@
+import html
+import streamlit as st
+import pandas as pd
+
+# ------------------------------------------------------------------------------------
+st.set_page_config(page_title="Zoo Data Model")
+st.markdown("""Here's a Data Model for the Zoo, created by Josh Goodall""")
+
+st.image('images/Josh-Data-Model.jpg', width=800)
